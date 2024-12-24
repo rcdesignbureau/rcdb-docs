3427adea18e7312ebb3e01523530bfb27e33681c
albonec <agbonecchi@gmail.com>
2024-12-23 19:44:08 -0800

    new pdf docs and reformatting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,9 +7,9 @@ import datetime as dt
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'RC Design Bureau'
+project = 'RCDB Docs'
 copyright = f"{dt.datetime.now().strftime('%Y')}, RC Design Bureau" # Updates the copyright to the current year
-author = "Alessandro Bonecchi, Antonio O'Hara, Eddie O'Hara, and Johann Webber at RC Design Bureau"
+author = "Alessandro Bonecchi, Antonio O'Hara, Eddie O'Hara"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -17,6 +17,7 @@ author = "Alessandro Bonecchi, Antonio O'Hara, Eddie O'Hara, and Johann Webber a
 extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
+language = 'en'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
@@ -24,3 +25,13 @@ exclude_patterns = []
 html_theme = 'furo'
 html_logo = 'assets/logo.png'
 html_favicon = 'assets/favicon.ico'
+
+# -- Options for LaTeX output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-latex-output
+latex_engine = 'pdflatex'
+latex_documents = [('index_latex', 'rcdb-docs.tex', 'RC Design Bureau Documentation', "Alessandro Bonecchi, Antonio O'Hara, and Eddie O'Hara", 'manual', False)]
+latex_elements = {
+    'documentclass': 'article',
+    'papersize': 'letterpaper',
+    'extraclassoptions': 'openany'
+}
---
+++ docs/source/selfdriving/selfdriving.rst
@@ -0,0 +1,14 @@
+==================
+Self-Driving Car Documentation
+==================
+
+**This is the section of our documentation dedicated to our pilot project, a self-driving RC-based model off-road race car. Check back again in the future for updates!**
+
+Contents
+========
+| `Install Guide <./install_guide.html>`_
+| `Setup Guide <./setup_guide.html>`_
+| `Hardware Troubleshooting <./hardware_troubleshooting.html>`_
+| `Software Troubleshooting <./software_troubleshooting.html>`_
+| `Our Truck <./our_truck.html>`_
+
